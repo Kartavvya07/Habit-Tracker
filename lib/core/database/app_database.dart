606322319d7f122056610af7e9286a12188a5050
Kartavya Sakaria <your.real.email@example.com>
2026-07-24 19:53:35 +0530

    chore: prepare drift database

--- lib/core/database/app_database.dart
+++ lib/core/database/app_database.dart
@@ -1,14 +1,10 @@
 import 'package:drift/drift.dart';
-import 'connection/open_connection.dart';
 
 part 'app_database.g.dart';
 
-/// Central Drift SQLite database class for the application infrastructure.
 @DriftDatabase(tables: [])
 class AppDatabase extends _$AppDatabase {
-  AppDatabase(String dbName) : super(openConnection(dbName));
-
-  AppDatabase.forTesting(super.connection);
+  AppDatabase(super.e);
 
   @override
   int get schemaVersion => 1;
